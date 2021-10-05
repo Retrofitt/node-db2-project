c37fe661524190bb1d942909f473610939de0f66
Retrofitt <rafaelmendozajr94@gmail.com>
2021-10-05 10:34:15 -0700

    done

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,77 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model')
+const vinValidator = require('vin-validator')
+
+
+const checkCarId = async (req, res, next) => {
+  try{
+    const car = await Cars.getById(req.params.id)
+    if(!car){
+      res.status(404).json({
+        message: `Car with id ${req.params.id} is not found`
+      })
+    }else{
+      next()
+    }
+  }catch (err){
+    res.status(err.status || 500).json({
+      customMessage:"An error has occurred",
+      message: err.message,
+      stack: err.stack
+    })
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const {vin, make, model, mileage} =  req.body
+  if(!vin){
+    res.status(400).json({
+      message: "vin is missing"
+    })
+  }if(!make){
+    res.status(400).json({
+      message: "make is missing"
+    })
+  }if(!model){
+    res.status(400).json({
+      message: "model is missing"
+    })
+  }if(!mileage){
+    res.status(400).json({
+      message: "mileage is missing"
+    })
+  }else{
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  const {vin} = req.body
+  const isValidVin = vinValidator.validate(vin)
+  if(isValidVin){
+    next()
+  }else{
+    res.status(400).json({
+      message: `vin ${vin} is invalid`
+    })
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  const {vin} = req.body
+  const cars = await Cars.getAll()
+  const unique = cars.filter(cars=>cars.vin === vin)
+  if(unique.length < 1){
+    next()
+  }else{
+    res.status(400).json({
+      message: `vin ${vin} already exists`
+    })
+  }
+}
+
+module.exports = {
+  checkCarId, 
+  checkCarPayload, 
+  checkVinNumberValid, 
+  checkVinNumberUnique
 }
